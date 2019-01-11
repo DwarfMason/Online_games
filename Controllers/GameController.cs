24f1a303490df395ad90dcfab9d666ad4feead2c
t1meshift <tarakanama@mail.ru>
2019-01-11 22:50:25 +1000

    Quick fix

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -18,5 +18,10 @@ namespace WebApplication1.Controllers
             return
             View("~/Views/Game/Town.cshtml");
         }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
     }
 }
