a10a94678f0bfd3c6a319b13ba177e4079eee364
t1meshift <tarakanama@mail.ru>
2019-01-11 19:52:42 +1000

    Fix debug controller

--- Controllers/DebugController.cs
+++ Controllers/DebugController.cs
@@ -1,8 +1,8 @@
 using System.Threading.Tasks;
-using AdministratorProject.Game;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Game;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
