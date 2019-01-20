b81f7b6568f6eb7585796de5cc9776060e02c2d5
DwarfMason <33534160+dwarfmason@users.noreply.github.com>
2019-01-21 00:35:32 +1000

    PVE gaming added to debug

--- Controllers/DebugController.cs
+++ Controllers/DebugController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Game;
+using WebApplication1.Game.BaseClasses;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -38,6 +39,16 @@ namespace WebApplication1.Controllers
         {
             var cult = await cultivatordb.GetCultivator(User.Identity.Name);
             cult.Gold = 0;
+            await cultivatordb.Update(cult);
+            return RedirectToAction("Index", "Debug");
+        }
+        
+        [Authorize]
+        public async Task<ActionResult> FightMob()
+        {
+            var cult = await cultivatordb.GetCultivator(User.Identity.Name);
+            var location = new СEventLocation();
+            location.CreateAdventure(cult);
             await cultivatordb.Update(cult);
             return RedirectToAction("Index", "Debug");
         }
